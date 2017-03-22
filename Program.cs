1842f09231bed7e0741120938bea0d18e11514db
farkadav <david.farkas.kn@gmail.com>
2017-03-22 01:16:09 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleApp2
+{
+    class Solution
+    {
+        static Spot[,] MakeReasonableGrid(int r, int c, int pacman_r, int pacman_c, int food_r, int food_c, String[] grid)
+        {
+            Spot[,] Grid = new Spot[r, c];
+            int j_index = 0;
+            foreach (string item in grid)
+            {
+                char[] helper = item.ToCharArray();
+                for (int i = 0; i < helper.Length; i++)
+                {
+                    if (helper[i].ToString() == "%")
+                    {
+                        Grid[j_index, i] = new Spot(j_index, i, false);
+                        continue;
+                    }
+                    if (helper[i].ToString() == "-")
+                    {
+                        Grid[j_index, i] = new Spot(j_index, i, true);
+                        continue;
+                    }
+                    else
+                    {
+                        Grid[j_index, i] = new Spot(j_index, i, true);
+                    }
+                }
+                j_index++;
+            }
+
+
+            for (var i = 0; i < c; i++)
+            {
+                for (var j = 0; j < r; j++)
+                {
+                    Grid[j, i].addNeighbors(Grid, c, r);
+                }
+            }
+
+            return Grid;
+        }
+
+
+        static void dfs(int r, int c, int pacman_r, int pacman_c, int food_r, int food_c, String[] grid)
+        {
+            //Your logic here
+            Stack<Spot> openList = new Stack<Spot>();
+            List<Spot> path = new List<Spot>();
+            List<Spot> expandedNodes = new List<Spot>();
+
+            //make reasonable grid and add neighbors
+            Spot[,] Grid = MakeReasonableGrid(r, c, pacman_r, pacman_c, food_r, food_c, grid);
+            
+            var start = Grid[pacman_r, pacman_c];
+            var end = Grid[food_r, food_c];
+
+            openList.Push(start);
+
+            while (openList.Count > 0)
+            {
+                var v = openList.Pop();
+                expandedNodes.Add(v);
+
+                if (v.Equals(end))
+                {
+                    path.Add(v);
+                    break;
+                }
+                if (!v.discovered)
+                {                    
+                    path.Add(v);
+                    v.discovered = true;
+                    foreach (var item in v.neighbors)
+                    {
+                        if (item.accessible & !item.discovered)
+                        {
+                            openList.Push(item);
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(expandedNodes.Count);
+            foreach (var item in expandedNodes)
+            {
+                Console.WriteLine(item.I + " " + item.J);
+            }
+            Console.WriteLine(path.Count - 1);
+            foreach (var item in path)
+            {
+                Console.WriteLine(item.I + " " + item.J);
+            }
+
+        }
+        static void Main(String[] args)
+        {
+            int r, c;
+            int pacman_r, pacman_c;
+            int food_r, food_c;
+
+            String pacman = Console.ReadLine();
+            String food = Console.ReadLine();
+            String pos = Console.ReadLine();
+
+            String[] pos_split = pos.Split(' ');
+            String[] pacman_split = pacman.Split(' ');
+            String[] food_split = food.Split(' ');
+
+            r = Convert.ToInt32(pos_split[0]);
+            c = Convert.ToInt32(pos_split[1]);
+
+            pacman_r = Convert.ToInt32(pacman_split[0]);
+            pacman_c = Convert.ToInt32(pacman_split[1]);
+
+            food_r = Convert.ToInt32(food_split[0]);
+            food_c = Convert.ToInt32(food_split[1]);
+
+            String[] grid = new String[r];
+
+            for (int i = 0; i < r; i++)
+            {
+                grid[i] = Console.ReadLine();
+            }
+
+
+            dfs(r, c, pacman_r, pacman_c, food_r, food_c, grid);
+        }
+    }
+}
---
+++ Spot.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    class Spot
+    {
+       
+        public Spot(int i, int j, Boolean Accessible)
+        {
+            I = i;
+            J = j;
+            f = 0;
+            g = 0;
+            h = 0;
+            neighbors = new List<Spot>();
+            accessible = Accessible;
+            discovered = false;
+        }
+
+        public int I { get; set; }
+        public int J { get; set; }
+        public double f { get; set; }   //cost function
+        public double h { get; set; }   // heuristics
+        public double g { get; set; }   //cost
+        public List<Spot> neighbors { get; set; }
+        public Boolean accessible { get; set; }
+        public Boolean discovered { get; set; }
+
+        public void addNeighbors(Spot[,] grid, int c, int r)
+        {
+            int i = I;
+            int j = J;
+            if (i < r-1)    neighbors.Add(grid[i + 1, j]);          
+            if (i > 0)      neighbors.Add(grid[i - 1, j]);       
+            if (j > 0 )     neighbors.Add(grid[i, j - 1]);
+            if (j < c - 1)  neighbors.Add(grid[i, j + 1]);
+           
+        }   
+    }
+}
